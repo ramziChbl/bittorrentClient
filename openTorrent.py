c8072f19ad0f0c8a9be58bd2988780630353a2f8
seriousRamzi <ramzy.chbl@gmail.com>
2020-05-23 16:10:31 +0100

    Added torrent file description overview

--- openTorrent.py
+++ openTorrent.py
@@ -1,5 +1,70 @@
-import asyncio, bencode, pprint
+import asyncio, bencode, pprint, json
 
+def describeTorrentDict(decodedDict):
+	dictKeys = decodedDict.keys()
+	print('Keys :', end='')
+	for key in dictKeys:
+		print(key, end=', ')
+	print()
+	
+	if 'announce' in dictKeys:
+		print('  announce = ' + decodedDict['announce'])
+
+	if 'announce-list' in dictKeys:
+		print('  announce-list = ' + str(decodedDict['announce-list']))
+
+	if 'creation date' in dictKeys:
+		print('  creation date = ' + str(decodedDict['creation date']))
+
+	if 'created by' in dictKeys:
+		print('  created by = ' + decodedDict['created by'])
+
+	if 'comment' in dictKeys:
+		print('  comment = ' + decodedDict['comment'])
+
+
+	if 'info' in dictKeys:
+		print('  info : ', end='')
+		for key in  decodedDict['info'].keys():
+			print(key, end=', ')
+		print()
+
+		print('    name = ' + decodedDict['info']['name'])
+		print('    piece length = ' + str(decodedDict['info']['piece length']))
+		print('    pieces number = ' + str(len(decodedDict['info']['pieces'])))
+		if 'files' in decodedDict['info'].keys():
+			print('    Multi-file')
+			print('    Files :')
+			for file in decodedDict['info']['files']:
+				print('        length = ' + str(file['length']))
+				delimiter = '/'
+				path = delimiter.join(file['path'])
+				print('        path = ' + path)
+				print()
+			
+			pp = pprint.PrettyPrinter()
+			#pp.pprint(decodedDict['info']['files'])
+		else:
+			print('    Single-file')
+			print('    length = ' + str(decodedDict['info']['length']))
+			#pp = pprint.PrettyPrinter()
+			#pp.pprint(decodedDict['info']['pieces'])
+
+	pass
+
+
+
+def readTorrentFile(filePath):
+	print('Reading file : ' + filePath)
+	decodedDict = bencode.bread(filePath)
+	describeTorrentDict(decodedDict)
+	
+	print()
+
+#readTorrentFile('file.torrent')
+readTorrentFile('file2.torrent')
+
+'''
 pp = pprint.PrettyPrinter()
 
 torrent = bencode.bread('file2.torrent')
@@ -10,6 +75,4 @@ print(torrent['comment'])
 print(torrent['info']['name'])
 print(torrent['info']['length'])
 print(torrent['info']['piece length'])
-
-#print(torrent['info'].keys())
-#pp.pprint(bencode.bread('file.torrent')['announce'])
+'''
