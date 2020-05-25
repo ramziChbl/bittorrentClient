3e882f91134e6e7c86f9582c8d59c47f444c4bf4
ramziChbl <ramzy.chbl@gmail.com>
2020-05-25 18:24:21 +0100

    Can extrace peers from compact response

--- openTorrent.py
+++ openTorrent.py
@@ -1,12 +1,26 @@
 import asyncio, bencode, pprint, json, requests, hashlib
 from torrentFile import TorrentFile
 
+
+def extractPeers(byteString):
+	peer = {}
+	peers = []
+	i = 0
+	while i<len(byteString):
+		peer = {}
+		peer['ip'] = str(byteString[i]) + '.' + str(byteString[i+1]) + '.' + str(byteString[i+2]) + '.' + str(byteString[i+3])
+		peer['port'] = byteString[i+4] * 255 + byteString[i+5]
+		peers.append(peer)
+		i += 6
+	return peers
+
+
 tf2 = TorrentFile('torrents/file2.torrent')
-tf2.describe()
+#tf2.describe()
 
 
 tf = TorrentFile('torrents/CentOS-8.1.1911-x86_64-boot.torrent')
-tf.describe()
+#tf.describe()
 getParameters = {
 	'info_hash' : tf2.infoHash,
 	'peer_id' : 'hellohellohellohello',
@@ -16,16 +30,20 @@ getParameters = {
 	'left' : tf2.info['length']# str(metainfo['info']['length'])
 }
 
-print(getParameters)
+#print(getParameters)
 
 response = requests.get(
     tf2.announce,
     params=getParameters,
 )
 
-print(response)
+#print(response)
 print(response.url)
-print(response.content)
+decodedContent = bencode.decode(response.content)
+print(decodedContent)
+print(type(decodedContent['peers']))
+#pp = pprint.PrettyPrinter()
+#pp.pprint(decodedContent)
 
 getParameters['info_hash'] = tf.infoHash
 l = 0
@@ -33,12 +51,16 @@ for f in tf.info['files']:
 	l += f['length']
 
 getParameters['left'] = l
-response = requests.get(
-	tf.announce, 
-	params = getParameters
-	)
+response = requests.get(tf.announce, params = getParameters)
 
 print()
-print(response)
+#print(response)
 print(response.url)
-print(response.content)
+#print(response.content)
+decodedContent = bencode.decode(response.content)
+print(decodedContent)
+print(type(decodedContent['peers']))
+if isinstance(decodedContent['peers'], bytes):
+	peers = extractPeers(decodedContent['peers'])
+	print(peers)
+#print(decodedContent['peers'])
