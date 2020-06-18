cc9323f7be7139494fd55ea2c154faf45a465c98
ramziChbl <gr_chikhbled@esi.dz>
2020-06-18 11:47:59 +0100

    testing connections

--- tcpConnect/tryConnect.py
+++ tcpConnect/tryConnect.py
@@ -1,5 +1,20 @@
 import socket, bencode
 
+
+def parseHandshake(data):
+	handshakeDict = {
+		'pstrlen' : data[0],
+	}
+
+	handshakeDict['pstr'] = data[1:handshakeDict['pstrlen']+1]
+
+	data = data[handshakeDict['pstrlen']+1:]
+	handshakeDict['reserved'] = data[:8]
+	data = data[8:]
+	handshakeDict['info_hash'] = data[:20]
+	handshakeDict['peer_id'] = data[20:]
+	return handshakeDict
+
 #handshakeMessage = b'\x13BitTorrent protocol\x00\x00\x00\x00\x00\x00\x00\x00{\xcb\xc3B\xefF\x8f\x05\xb4\x07\x1f[\xc3\x0b;j\xa2.\xe3chellohellohellohello'
 handshakeMessage = b'\x13BitTorrent protocol\x00\x00\x00\x00\x00\x00\x00\x00\xc4O\x93\x1b\x1a9\x86\x85\x12B\xd7U\xd0\xacF\xe9\xfa<]2hellohellohellohello'
 
@@ -22,3 +37,8 @@ s.close()
 print('Connection closed')
  
 print("Received data:" + str(data))
+print(type(data))
+print(len(data))
+print(parseHandshake(data))
+
+
