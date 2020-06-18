5bcb53129e9f984eb1acbf4b79e3957ee23fa979
ramziChbl <gr_chikhbled@esi.dz>
2020-06-18 12:18:36 +0100

    receiving bitfield

--- tcpConnect/tryConnect.py
+++ tcpConnect/tryConnect.py
@@ -1,5 +1,4 @@
-import socket, bencode
-
+import socket, bencode, struct
 
 def parseHandshake(data):
 	handshakeDict = {
@@ -7,7 +6,6 @@ def parseHandshake(data):
 	}
 
 	handshakeDict['pstr'] = data[1:handshakeDict['pstrlen']+1]
-
 	data = data[handshakeDict['pstrlen']+1:]
 	handshakeDict['reserved'] = data[:8]
 	data = data[8:]
@@ -15,30 +13,44 @@ def parseHandshake(data):
 	handshakeDict['peer_id'] = data[20:]
 	return handshakeDict
 
-#handshakeMessage = b'\x13BitTorrent protocol\x00\x00\x00\x00\x00\x00\x00\x00{\xcb\xc3B\xefF\x8f\x05\xb4\x07\x1f[\xc3\x0b;j\xa2.\xe3chellohellohellohello'
+def parseMessage(message):
+	messageDict = {
+		'length' : int.from_bytes(message[:4], byteorder='big'),
+		'id' : message[4]
+	}
+
+	messageDict['payload'] = message[5:messageDict['length'] - 1]
+	return messageDict
+
+
 handshakeMessage = b'\x13BitTorrent protocol\x00\x00\x00\x00\x00\x00\x00\x00\xc4O\x93\x1b\x1a9\x86\x85\x12B\xd7U\xd0\xacF\xe9\xfa<]2hellohellohellohello'
 
 BUFFER_SIZE = 69
 
-TCP_IP = '67.241.66.89'
-TCP_IP = '172.220.86.135'
+#TCP_IP = '67.241.66.89'
+#TCP_IP = '172.220.86.135'
 TCP_IP = '50.38.32.181'
 TCP_PORT = 51413
  
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#socket.socket()
+
 s.connect((TCP_IP, TCP_PORT))
 print('Connected')
 s.send(handshakeMessage)
 print('Handshake sent')
 data = s.recv(BUFFER_SIZE)
 print('Received data')
+message = s.recv(256)
+print('Received message')
+
+
 s.close()
 print('Connection closed')
  
-print("Received data:" + str(data))
-print(type(data))
-print(len(data))
+print("Received data:")
 print(parseHandshake(data))
 
+print('Received message')
+print(message)
+print(parseMessage(message))
 
